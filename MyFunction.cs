cab847ba144e80ad9a9313f9d5c36274d938257e
probovod1983 <80003510+probovod1983@users.noreply.github.com>
2021-05-02 21:50:58 +0300

    Add files via upload

---
+++ MyFunction.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace FuncChange
+{ // Диковицкий В.Б.
+    class MyFunction
+    {
+        public void Table(fun1 F, double x, double b)
+        {
+            Console.WriteLine("----- X ----- Y -----");
+            while (x <= b)
+            {
+                Console.WriteLine("| {0,8:0.000} | {1,8:0.000} |", x, F(x, b));
+                x += 1;
+            }
+            Console.WriteLine("---------------------");
+        }
+        public delegate double fun1(double a, double x);
+        public double MyFunc1(double a, double x)
+        {
+            return a * x * x;
+        }
+
+        public double MyFunc2(double a, double x)
+
+        {
+            return a * Math.Sin(x);
+             
+    }
+          
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace FuncChange
+{ // Диковицкий В.Б.
+  //Изменить программу вывода таблицы функции так, чтобы можно было передавать 
+  //функции типа double (double, double). Продемонстрировать работу на функции с функцией a*x^2 и функцией a*sin(x).
+
+    class Program {
+       
+        public static void Main()
+        {
+            MyFunction function = new MyFunction();
+                     
+            Console.WriteLine("Таблица функции MyFunc1:");
+            function.Table(new MyFunction.fun1(function.MyFunc1), -2, 2);
+            Console.WriteLine("Таблица функции MyFunc2:");
+            function.Table(new MyFunction.fun1(function.MyFunc2), -2, 2);
+            Console.ReadLine();
+        }
+        
+       
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
